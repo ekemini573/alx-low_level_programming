8ee88c97f9dfc691df383ea402f13063b9a53d9f
Ekemini Godwin Etokakpan <ekeminigodwin234@gmail.com>
2022-09-10 04:43:22 -0700

    first if statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,14 +16,14 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("is positive");
+printf("is positive %d\n", &n);
 }
 else if (n == 0)
 {
-printf("is zero");
+printf("is zero %d\n", &n);
 }
 else
-printf("is negative");
+printf("is negative %d\n", &n);
 
 return (0);
 }

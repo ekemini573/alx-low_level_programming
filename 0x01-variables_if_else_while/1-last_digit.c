cf5301f63ce1cc823ff0693b2902b18684e21872
Ekemini Godwin Etokakpan <ekeminigodwin234@gmail.com>
2022-09-10 06:29:24 -0700

    second if statement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,15 +17,15 @@ n = rand() - RAND_MAX / 2;
 last_digit = n % 10;
 printf("Last digit of %d is", n);
 
-if (n > 5)
+if (last_digit > 5)
 {
 printf(" %d and is greater than 5\n", last_digit);
 }
-else if (n == 0)
+else if (last_digit == 0)
 {
 printf(" %d and is 0\n", last_digit);
 }
-else if (n < 6 && n != 0)
+else if (last_digit < 6 && n != 0)
 {
   printf(" %d and is less than 6 and not 0\n", last_digit);
 }

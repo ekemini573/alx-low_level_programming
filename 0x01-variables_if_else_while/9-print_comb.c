ff6f0d9f757c833f7c06b989640d3d39496990bf
Ekemini Godwin Etokakpan <ekeminigodwin234@gmail.com>
2022-09-11 09:20:08 -0700

    final correction

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,19 +8,17 @@
 
 int main(void)
 {
-  int x ='0';
-  while (x <= '9')
-    {
-
-      putchar(x);
-      if (x != '9')
-
-	{
-	  putchar(',');
-	  putchar(' ');
-	}
-      x++;
-    }
-  putchar('\n');
-  return (0);
+int x = '0';
+while (x <= '9')
+{
+putchar(x);
+if (x != '9')
+{
+putchar(',');
+putchar(' ');
+}
+x++;
+}
+putchar('\n');
+return (0);
 }

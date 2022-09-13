eef1ad8d4de66d2eee374a9567e425baa962dc61
Ekemini Godwin Etokakpan <ekeminigodwin234@gmail.com>
2022-09-13 14:46:34 -0700

    print alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include <main.h>
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ *
+ */
+void print_alphabet_x10(void)
+{
+int i;
+char a;
+for (i = 1; i <= 10; i++)
+{
+for (a = 'a'; a <= 'z'; a++)
+{
+_putchar(a);
+}
+}
+_putchar('\n');
+}

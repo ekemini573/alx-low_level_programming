f8d016b074d90688f980ce77f535e2793cb7f36b
Ekemini Godwin Etokakpan <ekeminigodwin234@gmail.com>
2022-09-13 16:19:55 -0700

    convert to lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - check the code.
+ * @c: caracter recibo por main.
+ * Return: 1  is lowercase
+ */
+
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+return (0);
+}

9fea3c8d8c81feb6a3f6947880d2c67c197a78fe
Ekemini Godwin Etokakpan <ekeminigodwin234@gmail.com>
2022-09-14 12:11:53 -0700

    print last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - Entry point
+ * @l: last digit is possible
+ * Return: last digit (Success)
+ */
+int print_last_digit(int l)
+{
+int lastdigit = l % 10;
+return (lastdigit);
+}

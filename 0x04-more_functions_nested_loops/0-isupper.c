47f912c998f060689c1b69cd1b2e370ba86f1dfc
Ekemini Godwin Etokakpan <ekeminigodwin234@gmail.com>
2022-09-16 11:34:58 -0700

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for lowercase character.
+ * @c: caracter recibo por main.
+ * Return: 1 if c is uppercase
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}else if (c >= 'a' && c <= 'z')
+{
+return (0);
+}
+}

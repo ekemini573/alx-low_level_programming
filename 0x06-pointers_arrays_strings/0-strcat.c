123c7a7445e11617841fd6e895194d6103986735
Ekemini Godwin Etokakpan <ekeminigodwin234@gmail.com>
2022-09-23 13:36:24 -0700

    concatination

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,6 @@
 #include "main.h"
-
 /**
- * _strncat - appends src to the dest string
+ * _strcat - appends src to the dest string
  * @dest: string to append by src
  * @src: string to append to dest
  * Return: address of dest

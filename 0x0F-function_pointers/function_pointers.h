3d69dcf839f55bbbd9b3712f0eca86142f2f2ccd
Ekemini Godwin Etokakpan <ekeminigodwin234@gmail.com>
2022-10-12 14:22:34 -0700

    putchar added

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,9 +2,4 @@ void print_name(char *name, void (*f)(char *));
 void _putchar(char c);
 int int_index(int *array, int size, int (*cmp)(int));
 void array_iterator(int *array, size_t size, void (*action)(int));
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
+
